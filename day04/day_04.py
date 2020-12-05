6be4a0f783ce6d2ae1a7e62623d6231462017855
Sean Ellis <sellis@twitter.com>
2020-12-05 11:28:45 -0800

    Day 04

---
+++ day04/day_04.py
@@ -0,0 +1,34 @@
+TEST_INPUT = """ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in""".split('\n')
+
+
+def get_input():
+    with open('input.txt') as input_file:
+        return input_file.read().split('\n')
+
+def valid_passport(person):
+    print(person)
+
+def count_valid_passports(info):
+    count = 0
+    person = ''
+    for text in info:
+        if not text:
+            count += 1 if valid_passport(person) else 0
+            person = ''
+        else:
+            person += f' {text}'
+    count += 1 if valid_passport(person) else 0
+
+valid_passports = count_valid_passports(TEST_INPUT)
