6f3aee9898e72548b005543153ef27ddf256ebf5
Sean Jain Ellis <sellis@bandarji.com>
2022-11-29 08:15:09 -1000

    Rust as mods

--- 2015/Rust/aoc2015/src/lib.rs
+++ 2015/Rust/aoc2015/src/lib.rs
@@ -1,19 +1,3 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
-
-pub fn read_file(filename: &str) -> String {
-    let path = Path::new(filename);
-    let expanded = path.display();
-
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("Could not open {}: {}", expanded, why),
-        Ok(file) => file,
-    };
-
-    let mut s = String::new();
-    match file.read_to_string(&mut s) {
-        Err(why) => panic!("Could not read {}: {}", expanded, why),
-        Ok(_file) => s,
-    }
-}
+pub mod input;
+pub mod day01;
+pub mod day02;
