93369e2c16342b3adb26058b0b2d122db7a8f0f2
Sean Jain Ellis <sellis@bandarji.com>
2022-12-09 10:15:09 -1000

    Rust Day04

--- 2022/Rust/aoc2022/src/main.rs
+++ 2022/Rust/aoc2022/src/main.rs
@@ -2,9 +2,10 @@ use aoc2022::input::read_file;
 use aoc2022::day01::{day011, day012};
 use aoc2022::day02::{day021, day022};
 use aoc2022::day03::{day031, day032};
+use aoc2022::day04::{day041, day042};
 
 fn day01() {
-    let filename: &str = "day01_input.txt";
+    let filename: &str = "input_day01.txt";
     let input1 = read_file(filename);
     let input2 = input1.clone();
     println!("Answer [1-1]: {}", day011(input1));
@@ -27,8 +28,17 @@ fn day03() {
     println!("Answer [3-2]: {}", day032(input2));
 }
 
+fn day04() {
+    let filename: &str = "input_day04.txt";
+    let input1 = read_file(filename);
+    let input2 = input1.clone();
+    println!("Answer [4-1]: {}", day041(input1));
+    println!("Answer [4-2]: {}", day042(input2));
+}
+
 fn main() {
     day01();
     day02();
     day03();
+    day04();
 }
