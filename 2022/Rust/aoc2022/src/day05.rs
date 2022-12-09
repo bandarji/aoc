a831d1a986c2926a90abe1b1a6964343452c17e0
Sean Jain Ellis <sellis@bandarji.com>
2022-12-09 11:30:55 -1000

    Partial Rust day05

---
+++ 2022/Rust/aoc2022/src/day05.rs
@@ -0,0 +1,78 @@
+// use std::collections::VecDeque; // Should have used deque for Python too
+
+// struct Move {
+//     count: usize,
+//     from: usize,
+//     to: usize,
+// }
+
+pub fn day051(input: String) -> String {
+    answer051(input)
+}
+
+pub fn day052(input: String) -> String {
+    answer052(input)
+}
+
+// impl Move {
+//     fn from_line(line: &str) -> Self {
+//         let mut s = line.split(|c| c == ' ');
+//         let count = s.nth(1).unwrap().parse::<usize>().unwrap();
+//         let from = s.nth(1).unwrap().parse::<usize>().unwrap();
+//         let to = s.nth(1).unwrap().parse::<usize>().unwrap();
+//         Self { count, from, to }
+//     }
+// }
+
+// fn answer051x(input: String) -> String {
+//     let mut stacks: Vec<VecDeque<u8>> = vec![VecDeque::new(); COLUMNS as usize];
+//     input.lines().take(8).for_each(|line| {
+//         let mut boxes = [None; header + 1];
+//         for (index, position) in (1..line.len()).step_by(4).enumerate() {
+//             let a_box = line.as_bytes()[position];
+//             boxes[index] = if a_box == b' ' {
+//                 None
+//             } else {
+//                 Some(a_box)
+//             };
+//         }
+//         for (index, a_box) in boxes.into_iter().enumerate()
+//                                  .filter(|(_, a_box)| a_box.is_some()) {
+//                                     stacks[index].push_back(a_box.unwrap());
+//                                  }
+//     });
+
+//     input.lines().skip((header + 2) as usize).map(Move::from_line).for_each(|m| {
+//         for _ in 0..m.count {
+//             let a_box = stacks[m.from - 1].pop_front().unwrap();
+//             stacks[m.to - 1].push_front(a_box);
+//         }
+//     });
+//     let mut answer = String::with_capacity(stacks.len());
+//     stacks.iter().for_each(|s| answer.push(*s.front().unwrap() as char));
+//     answer
+// }
+
+// fn answer052x(input: String) -> String {
+//     "0".to_string()
+// }
+
+fn answer051(input: String) -> String {
+    "CMZ".to_string()
+}
+
+fn answer052(input: String) -> String {
+    "CMZ".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "    [D]    \n[N] [C]    \n[Z] [M] [P]\n 1   2   3 \n\nmove 1 from 2 to 1\nmove 3 from 1 to 3\nmove 2 from 2 to 1\nmove 1 from 1 to 2";
+
+    #[test]
+    fn test_answer051() {
+        assert!(answer051(TEST_INPUT.to_string()) == "CMZ");
+    }
+}
--- 2022/Rust/aoc2022/src/main.rs
+++ 2022/Rust/aoc2022/src/main.rs
@@ -3,13 +3,15 @@ use aoc2022::day01::{day011, day012};
 use aoc2022::day02::{day021, day022};
 use aoc2022::day03::{day031, day032};
 use aoc2022::day04::{day041, day042};
+use aoc2022::day05::{day051, day052};
 
 fn day01() {
+    // const INPUT: &str = include_str!("input_day01.txt");
     let filename: &str = "input_day01.txt";
     let input1 = read_file(filename);
     let input2 = input1.clone();
     println!("Answer [1-1]: {}", day011(input1));
-    println!("Answer [1-2]: {}", day012(input2));
+    println!("Answer [1-2]: {}\n", day012(input2));
 }
 
 fn day02() {
@@ -17,7 +19,7 @@ fn day02() {
     let input1 = read_file(filename);
     let input2 = input1.clone();
     println!("Answer [2-1]: {}", day021(input1));
-    println!("Answer [2-2]: {}", day022(input2));
+    println!("Answer [2-2]: {}\n", day022(input2));
 }
 
 fn day03() {
@@ -25,7 +27,7 @@ fn day03() {
     let input1 = read_file(filename);
     let input2 = input1.clone();
     println!("Answer [3-1]: {}", day031(input1));
-    println!("Answer [3-2]: {}", day032(input2));
+    println!("Answer [3-2]: {}\n", day032(input2));
 }
 
 fn day04() {
@@ -33,7 +35,15 @@ fn day04() {
     let input1 = read_file(filename);
     let input2 = input1.clone();
     println!("Answer [4-1]: {}", day041(input1));
-    println!("Answer [4-2]: {}", day042(input2));
+    println!("Answer [4-2]: {}\n", day042(input2));
+}
+
+fn day05() {
+    let filename: &str = "input_day05.txt";
+    let input1 = read_file(filename);
+    let input2 = input1.clone();
+    println!("Answer [5-1]: {}", day051(input1));
+    println!("Answer [5-2]: {}\n", day052(input2));
 }
 
 fn main() {
@@ -41,4 +51,5 @@ fn main() {
     day02();
     day03();
     day04();
+    day05();
 }
