3737ef75ec9a35e406d803d5929e0bf3d71418fc
Sean Jain Ellis <sellis@aurora.tech>
2023-05-24 12:57:24 -0700

    [Go] 2016 01

--- 2016/Go/d01/d01.go
+++ 2016/Go/d01/d01.go
@@ -2,122 +2,61 @@ package d01
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"strconv"
 	"strings"
 )
 
-type Santa struct {
-	Facing  int // N = 0, E = 1, S = 2, W = 3
-	X, Y    int
-	Visited map[string]bool
-	Part    int
-}
-
-func (s Santa) HasVisited(x, y int) bool {
-	if x == 0 && y == 0 {
-		return false
-	}
-	key := NormalizePosition(x, y)
-	if _, found := s.Visited[key]; found {
-		return true
-	} else {
-		return false
-	}
-}
-
-func (s *Santa) Turn(turn string) {
-	if turn == "L" {
-		switch s.Facing {
-		case 0:
-			s.Facing = 3
-		case 1:
-			s.Facing = 0
-		case 2:
-			s.Facing = 1
-		case 3:
-			s.Facing = 2
-		}
-	} else {
-		switch s.Facing {
-		case 0:
-			s.Facing = 1
-		case 1:
-			s.Facing = 2
-		case 2:
-			s.Facing = 3
-		case 3:
-			s.Facing = 0
-		}
-	}
-}
-
-func (s *Santa) Move() {
-	x, y := s.X, s.Y
-	switch s.Facing {
-	case 0:
-		y += 1
-	case 1:
-		x += 1
-	case 2:
-		y -= 1
-	case 3:
-		x -= 1
-	}
-	s.X = x
-	s.Y = y
-	key := NormalizePosition(x, y)
-	if _, found := s.Visited[key]; found {
-		if s.Part == 2 {
-			log.Printf("Insection: %d, %d: ", x, y)
-		}
-	}
-	s.Visited[key] = true
-}
-
-func (s Santa) Distance() int {
-	return Abs(0, s.X) + Abs(0, s.Y)
-}
-
-func Abs(n1, n2 int) (d int) {
-	d = n1 - n2
-	if d < 0 {
-		d *= -1
-	}
-	return
-}
+var Directions = [4]int{0, 1, 2, 3}
+var Deltas = [4][2]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
 
 func NormalizePosition(x, y int) string {
 	return fmt.Sprintf("%d,%d", x, y)
 }
 
-func Day(part int) int {
-	content, err := os.ReadFile("inputs/input_day01.txt")
-	if err != nil {
-		log.Fatalln("I/O", err)
-	}
-	return WalkCity(string(content), part)
-}
-
-func WalkCity(input string, part int) int {
-	santa := &Santa{0, 0, 0, map[string]bool{"0,0": true}, part}
-	for _, i := range strings.Fields(input) {
-		santa.Turn(string(i[0]))
-		for m := 0; m < atoi(i); m++ {
-			santa.Move()
-		}
-	}
-	return santa.Distance()
-}
-
-func atoi(s string) (n int) {
-	ns := ""
-	for _, c := range s {
-		if c >= 48 && c <= 57 {
-			ns += string(c)
-		}
-	}
-	n, _ = strconv.Atoi(ns)
+func ParseInstruction(instruction string) (turn string, steps int) {
+	fmt.Sscanf(instruction, "%1s%d", &turn, &steps)
 	return
+}
+
+func Turn(turn string, facing int) (direction int) {
+	delta := 0
+	if turn == "R" {
+		delta = 1
+	} else if turn == "L" {
+		delta = 3
+	}
+	direction = Directions[(facing+delta)%4]
+	return
+}
+
+func Distance(sx, sy, ex, ey int) int {
+	return Abs(sx, ex) + Abs(sy, ey)
+}
+
+func Abs(v1, v2 int) (a int) {
+	a = v2 - v1
+	if a < 0 {
+		a *= -1
+	}
+	return
+}
+
+func Day(input string, part int) int {
+	visited := map[string]bool{}
+	facing := 0 // 0 -> N, 1 -> E, 2 -> S, 3 -> W
+	x, y := 0, 0
+	position := NormalizePosition(x, y)
+	for _, instruction := range strings.Split(input, ", ") {
+		turn, steps := ParseInstruction(instruction)
+		facing = Turn(turn, facing)
+		for i := 0; i < steps; i++ {
+			x += Deltas[facing][0]
+			y += Deltas[facing][1]
+			position = NormalizePosition(x, y)
+			if part == 2 && visited[position] {
+				return Distance(0, 0, x, y)
+			}
+			visited[position] = true
+		}
+	}
+	return Distance(0, 0, x, y)
 }
--- 2016/Go/main.go
+++ 2016/Go/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"go2016/d01"
 )
 
 func main() {
-	log.Println("Day 01/01:", d01.Day(1))
-	log.Println("Day 01/02:", d01.Day(2))
+	content, err := os.ReadFile("inputs/input_day01.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("Day 01/01:", d01.Day(string(content), 1))
+	log.Println("Day 01/02:", d01.Day(string(content), 2))
 }
