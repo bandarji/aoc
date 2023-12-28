204c71c6d83c376c014a44f1e921b71c24c75892
Sean Jain Ellis <sellis@bandarji.com>
2023-12-28 11:15:38 -1000

    Hmm, gettin there. Need multipath visit tracking?

--- 2023/Go/d23/d23.go
+++ 2023/Go/d23/d23.go
@@ -1,8 +1,8 @@
 package d23
 
 import (
+	"fmt"
 	"log"
-	"slices"
 	"strings"
 )
 
@@ -34,112 +34,67 @@ type Point struct {
 	y, x int
 }
 
-type Plate struct {
-	d, y, x int
+type Place struct {
+	d int
+	p Point
 }
 
-var (
-	grid       []string
-	start, end Point
-	points     []Point
-	graph      map[Point]map[Point]int
-	visited    map[Point]bool
-)
-
-func InGrid(p Point) bool {
-	return p.y >= 0 && p.x >= 0 && p.y < len(grid) && p.x < len(grid[0])
+func InGrid(grid *[]string, y, x int) bool {
+	return y >= 0 && y < len(*grid) && x >= 0 && x < len((*grid)[0])
 }
 
-func ThreeNeighborPoints() {
-	points = []Point{start}
-	for y, row := range grid {
-		for x, char := range strings.Split(row, "") {
-			if char == "#" {
-				continue
-			}
-			nbr := 0
-			for _, np := range [4]Point{{y - 1, x}, {y + 1, x}, {y, x - 1}, {y, x + 1}} {
-				if InGrid(np) && grid[np.y][np.x] != '#' {
-					nbr++
-				}
-			}
-			if nbr > 2 {
-				points = append(points, Point{y, x})
-			}
-		}
-	}
-	points = append(points, end)
-}
-
-func BuildGraph() {
-	visited = map[Point]bool{}
-	graph = map[Point]map[Point]int{}
+func LongestHike(grid []string, start, end Point) (answer int) {
+	steps := []int{}
+	visited := map[Point]bool{}
 	dirs := map[byte][]Point{
 		'^': {{-1, 0}},
 		'v': {{1, 0}},
 		'<': {{0, -1}},
 		'>': {{0, 1}},
 		'.': {{-1, 0}, {1, 0}, {0, -1}, {0, 1}},
+		'#': {},
 	}
-	for _, point := range points {
-		graph[point] = map[Point]int{}
-	}
-	for _, p := range points {
-		sy, sx := p.y, p.x
-		stack := []Plate{{0, sy, sx}}
-		visited[Point{sy, sx}] = true
-		for len(stack) > 0 {
-			var plate Plate
-			sl := len(stack)
-			if sl == 1 {
-				plate = stack[0]
-				stack = []Plate{}
-			} else {
-				plate = stack[sl-1]
-				stack = stack[0 : sl-2]
-			}
-			d, y, x := plate.d, plate.y, plate.x
-			if d != 0 && slices.Contains(points, Point{y, x}) {
-				graph[Point{sy, sx}][Point{y, x}] = d
+	q := []Place{{0, start}}
+	for len(q) > 0 {
+		place := q[0]
+		q = q[1:]
+		d, sy, sx := place.d, place.p.y, place.p.x
+		for _, dp := range dirs[grid[sy][sx]] {
+			ny, nx := sy+dp.y, sx+dp.x
+			if !InGrid(&grid, ny, nx) || grid[ny][nx] == '#' || visited[Point{ny, nx}] {
 				continue
 			}
-			for _, dir := range dirs[grid[y][x]] {
-				ny, nx := y+dir.y, x+dir.x
-				if InGrid(Point{ny, nx}) && grid[ny][nx] != '#' && !visited[Point{ny, nx}] {
-					stack = append(stack, Plate{d + 1, ny, nx})
-					visited[Point{ny, nx}] = true
-				}
+			DisplayGrid(grid, visited)
+			q = append(q, Place{d + 1, Point{ny, nx}})
+			if ny == end.y && nx == end.x {
+				steps = append(steps, d+1)
 			}
 		}
+		visited[Point{sy, sx}] = true
 	}
+	log.Printf("steps=%+v", steps)
+	return
 }
 
-func DFS(p Point) int {
-	if p.y == end.y && p.x == end.x {
-		return 0
-	}
-	m := 0
-	visited[p] = true
-	for np, d := range graph[p] {
-		if !visited[np] {
-			if _, exists := graph[p][np]; !exists {
-				d = 0
+func DisplayGrid(grid []string, visited map[Point]bool) {
+	fmt.Println("")
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid[0]); x++ {
+			if visited[Point{y, x}] {
+				fmt.Print("O")
+			} else {
+				fmt.Print(string(grid[y][x]))
 			}
-			m = max(m, DFS(np)+d)
 		}
+		fmt.Println("")
 	}
-	delete(visited, p)
-	return m
+	log.Print("")
 }
 
 func Solve(input string, part int) (answer int) {
-	grid = strings.Split(input, "\n")
-	start = Point{0, strings.Index(grid[0], ".")}
-	end = Point{len(grid) - 1, strings.Index(grid[len(grid)-1], ".")}
-	ThreeNeighborPoints()
-	BuildGraph()
-	log.Printf("graph=%+v", graph)
-	visited = map[Point]bool{}
-	answer = DFS(start)
+	grid := strings.Split(input, "\n")
+	start := Point{0, strings.Index(grid[0], ".")}
+	end := Point{len(grid) - 1, strings.Index(grid[len(grid)-1], ".")}
+	answer = LongestHike(grid, start, end)
 	return
 }
