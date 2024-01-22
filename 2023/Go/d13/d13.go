c73a97b90b125c0e78085fed36855288694c8bf7
Sean Jain Ellis <sellis@bandarji.com>
2024-01-22 12:30:13 -1000

    2023 d13

--- 2023/Go/d13/d13.go
+++ 2023/Go/d13/d13.go
@@ -1,7 +1,6 @@
 package d13
 
 import (
-	"slices"
 	"strings"
 )
 
@@ -21,135 +20,81 @@ const TEST1 string = `#.##..##.
 ..##..###
 #....#..#`
 
-func Part1(patterns [][][]string) (sum int) {
-	for _, pattern := range patterns {
-		vert, hori := Mirrors(pattern)
-		sum += hori*100 + vert
-	}
-	return
-}
-
-func Part2(patterns [][][]string) (sum int) {
-	for _, pattern := range patterns {
-		vert, hori := Smudged(pattern)
-		sum += hori*100 + vert
-	}
-	return
-}
-
-func Smudged(pattern [][]string) (vert, hori int) {
-	changes := []int{}
-	count := 0
-	half := len(pattern) / 2
-	for i := 0; i < len(pattern[0])-1; i++ {
-		count = 0
-		for x := i; x >= 0; x-- {
-			if i+i+1-x > len(pattern[0])-1 {
-				break
-			}
-			for y := 0; y < len(pattern); y++ {
-				if pattern[y][x] != pattern[y][i+i+1-x] {
-					count++
-				}
-			}
-		}
-		if count == 1 {
-			changes = append(changes, i+1)
-		}
-	}
-	vert = MinimumDistance(changes, half)
-	changes = []int{}
-	for i := 0; i < len(pattern)-1; i++ {
-		count = 0
-		for y := 1; y >= 0; y-- {
-			if i+i+1-y > len(pattern)-1 {
-				break
-			}
-			for x := 0; x < len(pattern[0]); x++ {
-				if pattern[y][x] != pattern[i+i+1-y][x] {
-					count++
-				}
-			}
-		}
-		if count == 1 {
-			changes = append(changes, i+1)
-		}
-	}
-	hori = MinimumDistance(changes, half)
-	return
-}
-
-func MinimumDistance(sources []int, dist int) (minimum int) {
-	shortest := 1<<63 - 1
-	for _, source := range sources {
-		distance := source - dist
-		if distance < 0 {
-			distance *= -1 // absolute
-		}
-		if distance < shortest {
-			shortest = distance
-			minimum = source
-		}
-	}
-	return
-}
-
-func Mirrors(pattern [][]string) (vert, hori int) {
-	matches := []int{}
-	half := len(pattern) / 2
-	match := false
-	for i := 0; i < len(pattern)-1; i++ {
-		match = true
-		for y := i; y >= 0; y-- {
-			if i+i+1-y > len(pattern)-1 {
-				break
-			}
-			if !slices.Equal(pattern[y], pattern[i+i+1-y]) {
-				match = false
-				break
-			}
-		}
-		if match {
-			matches = append(matches, i+1)
-		}
-	}
-	hori = MinimumDistance(matches, half)
-	matches = []int{}
-	for i := 0; i < len(pattern[0])-1; i++ {
-		match = true
-		for x := i; x >= 0; x-- {
-			if i+i+1-x > len(pattern[0])-1 {
-				break
-			}
-			for y := 0; y < len(pattern); y++ {
-				if pattern[y][x] != pattern[y][i+i+1-x] {
-					match = false
-					break
-				}
-			}
-		}
-		if match {
-			matches = append(matches, i+1)
-		}
-	}
-	vert = MinimumDistance(matches, half)
-	return
-}
-
 func Solve(input string, part int) (answer int) {
-	patterns := [][][]string{}
-	pattern := [][]string{}
+	hori, vert := 0, 0
 	for _, block := range strings.Split(input, "\n\n") {
-		for _, line := range strings.Split(block, "\n") {
-			pattern = append(pattern, strings.Split(line, ""))
-		}
-		patterns = append(patterns, pattern)
-		pattern = [][]string{}
+		grid := strings.Split(block, "\n")
+		hori = FindMirror(grid, part)
+		vert = FindMirror(Transpose(grid), part)
+		answer += hori * 100
+		answer += vert
 	}
-	if part == 1 {
-		answer = Part1(patterns)
-	} else {
-		answer = Part2(patterns)
+	return
+}
+
+func FindMirror(grid []string, part int) int {
+	reflects := Reflects1
+	if part == 2 {
+		reflects = Reflects2
+	}
+	for i := 0; i < len(grid)-1; i++ {
+		if reflects(grid, i) {
+			return i + 1
+		}
+	}
+	return 0
+}
+
+func Reflects1(grid []string, start int) bool {
+	lo, up := start, start+1
+	for lo >= 0 && up < len(grid) {
+		if grid[lo] != grid[up] {
+			return false
+		}
+		lo--
+		up++
+	}
+	return true
+}
+
+func Fixable(s1, s2 string) bool {
+	diffs := 0
+	for i := 0; i < min(len(s1), len(s2)); i++ {
+		if s1[i] != s2[i] {
+			diffs++
+		}
+	}
+	return diffs == 1
+}
+
+func Reflects2(grid []string, start int) bool {
+	lo, up := start, start+1
+	fixed := false
+	for lo >= 0 && up < len(grid) {
+		if grid[lo] != grid[up] {
+			if fixed {
+				return false
+			} else if Fixable(grid[lo], grid[up]) {
+				fixed = true
+			} else {
+				return false
+			}
+		}
+		lo--
+		up++
+	}
+	return fixed
+}
+
+func Transpose(grid []string) (ngrid []string) {
+	ngrid = []string{}
+	s := ""
+	for x := 0; x < len(grid[0]); x++ {
+		for y := 0; y < len(grid); y++ {
+			s += string(grid[y][x])
+		}
+		ngrid = append(ngrid, s)
+		s = ""
 	}
 	return
 }
