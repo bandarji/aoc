e3e9b87a92a5970e608f76647adc3376939dd18a
Sean Jain Ellis <sellis@bandarji.com>
2024-02-25 12:46:56 -1000

    2023 23

--- 2023/Go/d23/d23.go
+++ 2023/Go/d23/d23.go
@@ -1,9 +1,7 @@
 package d23
 
 import (
-	"fmt"
-	"log"
-	"os"
+	"slices"
 	"strings"
 )
 
@@ -32,86 +30,114 @@ const TEST1 string = `#.#####################
 #####################.#`
 
 type Point struct {
-	x, y int
+	y, x int
 }
 
-var visited = make(map[Point]int)
+type Distance struct {
+	p Point
+	d int
+}
 
 func Endpoints(grid []string) (start, end Point) {
-	start = Point{strings.Index(grid[0], "."), 0}
-	end = Point{strings.Index(grid[len(grid)-1], "."), len(grid) - 1}
+	start = Point{0, strings.Index(grid[0], ".")}
+	end = Point{len(grid) - 1, strings.Index(grid[len(grid)-1], ".")}
 	return
 }
 
-func Hike(grid []string, start, facing Point) {
-	// log.Printf("visited=%#v", visited)
-	opMap := map[Point]byte{
-		{1, 0}:  '<',
-		{-1, 0}: '>',
-		{0, 1}:  '^',
-		{0, -1}: 'v',
+func Dirs(ch rune, part int) (dirs []Point) {
+	if part == 2 {
+		dirs = []Point{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+	} else {
+		switch ch {
+		case '^':
+			dirs = []Point{{-1, 0}}
+		case 'v':
+			dirs = []Point{{1, 0}}
+		case '<':
+			dirs = []Point{{0, -1}}
+		case '>':
+			dirs = []Point{{0, 1}}
+		case '.':
+			dirs = []Point{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+		}
 	}
-	current := start
-	currStep := visited[current]
-	for _, d := range [3]Point{facing, Left(facing), Right(facing)} {
-		np := Point{current.x + d.x, current.y + d.y}
-		if InGrid(grid, np) && grid[np.y][np.x] != '#' {
-			if opMap[d] == grid[np.y][np.x] {
+	return
+}
+
+func Hike(grid []string, start, end Point, part int) (answer int) {
+	points := []Point{start, end}
+	for r, row := range grid {
+		for c, ch := range row {
+			if ch == '#' {
 				continue
 			}
-			if cost, exists := visited[np]; !exists || cost < currStep+1 {
-				if cost > np.y*np.x*200 {
-					log.Printf("visited=%#v", visited)
-					log.Print("WTF")
-					os.Exit(1)
+			neighbors := 0
+			for _, np := range []Point{{r - 1, c}, {r + 1, c}, {r, c - 1}, {r, c + 1}} {
+				if np.y >= 0 && np.y < len(grid) && np.x >= 0 && np.x < len(grid[0]) && grid[np.y][np.x] != '#' {
+					neighbors++
 				}
-				visited[np] = currStep + 1
-				Hike(grid, np, d)
+			}
+			if neighbors > 2 {
+				points = append(points, Point{r, c})
 			}
 		}
 	}
-}
+	graph := map[Point]map[Point]int{}
+	for _, p := range points {
+		graph[p] = map[Point]int{}
+	}
 
-func InGrid(grid []string, p Point) bool {
-	return p.x >= 0 && p.y >= 0 && p.x < len(grid[0]) && p.y < len(grid)
-}
-func Left(p Point) Point  { return Point{p.y, -p.x} }
-func Right(p Point) Point { return Point{-p.y, p.x} }
-
-func NoSlopes(grid []string) (ngrid []string) {
-	ngrid = []string{}
-	tmp := []string{}
-	for _, line := range grid {
-		for _, ch := range line {
-			char := string(ch)
-			switch char {
-			case "#":
-				tmp = append(tmp, char)
-			case "^", "v":
-				tmp = append(tmp, ".")
-			default:
-				tmp = append(tmp, ".")
+	for _, p := range points {
+		stack := []Distance{{p, 0}}
+		visited := map[Point]bool{p: true}
+		for len(stack) > 0 {
+			plate := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			n, r, c := plate.d, plate.p.y, plate.p.x
+			if n != 0 && slices.Contains(points, Point{r, c}) {
+				graph[p][Point{r, c}] = n
+				continue
+			}
+			for _, np := range Dirs(rune(grid[r][c]), part) {
+				nr, nc := r+np.y, c+np.x
+				if nr >= 0 && nr < len(grid) && nc >= 0 && nc < len(grid[0]) && grid[nr][nc] != '#' && !visited[Point{nr, nc}] {
+					stack = append(stack, Distance{Point{nr, nc}, n + 1})
+					visited[Point{nr, nc}] = true
+				}
 			}
 		}
-		ngrid = append(ngrid, strings.Join(tmp, ""))
-		tmp = []string{}
 	}
+	// fmt.Println("Points")
+	// for _, p := range points {
+	// 	fmt.Printf("  %#v\n", p)
+	// }
+	// fmt.Println("\nGraph")
+	// for k, v := range graph {
+	// 	fmt.Printf("  %#v: %#v\n", k, v)
+	// }
+	seen := map[Point]bool{}
+	answer = DFS(graph, start, end, seen)
 	return
+}
+
+func DFS(graph map[Point]map[Point]int, point, end Point, seen map[Point]bool) (answer int) {
+	if point == end {
+		return 0
+	}
+	seen[point] = true
+	m := -1000000
+	for k, v := range graph[point] {
+		if !seen[k] {
+			m = max(m, DFS(graph, k, end, seen)+v)
+		}
+	}
+	delete(seen, point)
+	return m
 }
 
 func Solve(input string, part int) (answer int) {
-	visited = map[Point]int{}
 	grid := strings.Split(input, "\n")
 	start, end := Endpoints(grid)
-	// log.Printf("start=%#v, end=%#v", start, end)
-	visited[start] = 0
-	if part == 1 {
-		Hike(grid, start, Point{0, 1})
-	} else {
-		// grid = NoSlopes(grid)
-		fmt.Print(strings.Join(grid, "\n"), "\n")
-		Hike(grid, start, Point{0, 1})
-	}
-	answer = visited[end]
+	answer = Hike(grid, start, end, part)
 	return
 }
