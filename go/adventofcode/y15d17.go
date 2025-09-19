9e57b90ec7aeace693b2d42d1d43c074255c063f
Sean Jain Ellis <sellis@bandarji.com>
2025-09-19 11:48:08 -0700

    2015 Day 17: No Such Thing as Too Much

---
+++ go/adventofcode/y15d17.go
@@ -0,0 +1,65 @@
+package adventofcode
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Y15D17 struct{}
+
+func (d *Y15D17) GetInput(year, day int) string {
+	return readContent(formatFilename(year, day))
+}
+
+func (d *Y15D17) Part1(year, day int) string {
+	return fmt.Sprintf("Year=%d Day=%02d Part 1: %d", year, day, y15d17(d.GetInput(year, day), 150, 1))
+}
+
+func (d *Y15D17) Part2(year, day int) string {
+	return fmt.Sprintf("Year=%d Day=%02d Part 2: %d", year, day, y15d17(d.GetInput(year, day), 150, 2))
+}
+
+func y15d17(input string, liters int, part int) (answer int) {
+	containers := y15d17GetContainers(input)
+	combinations := y15d17GetCombinations(containers, 0, liters, []int{})
+	if part == 1 {
+		answer = len(combinations)
+	} else {
+		minimum := 0xffff0000
+		for _, combination := range combinations {
+			minimum = min(minimum, len(combination))
+		}
+		count := 0
+		for _, combination := range combinations {
+			if len(combination) == minimum {
+				count++
+			}
+		}
+		answer = count
+	}
+	return
+}
+
+func y15d17GetContainers(input string) (containers []int) {
+	for _, line := range strings.Split(input, "\n") {
+		containers = append(containers, strToInt(line))
+	}
+	return
+}
+
+func y15d17GetCombinations(containers []int, start int, remaining int, known []int) [][]int {
+	if remaining == 0 {
+		return [][]int{append([]int{}, known...)}
+	}
+	if remaining < 0 {
+		return nil
+	}
+	var goodCombinations [][]int
+	for i := start; i < len(containers); i++ {
+		known = append(known, i)
+		goodCombinations = append(goodCombinations, y15d17GetCombinations(containers, i+1, remaining-containers[i], known)...)
+		known = known[:len(known)-1]
+	}
+	return goodCombinations
+
+}
