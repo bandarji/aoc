472d2705065c98d79a398b58b795055108e24349
Sean Jain Ellis <sellis@bandarji.com>
2026-03-26 16:02:41 +0900

    Rewritten with a struct

--- go/adventofcode/y16d23.go
+++ go/adventofcode/y16d23.go
@@ -5,66 +5,95 @@ import (
 	"strings"
 )
 
-var y16d23Registers = map[string]int{"a": 7}
+type y16d23Computer struct {
+	registers    map[string]int
+	instructions []string
+	cursor, size int
+}
+
+func (c *y16d23Computer) executeInstruction() {
+	instruction := c.instructions[c.cursor]
+	fields := strings.Fields(instruction)
+	switch fields[0] {
+	case "cpy":
+		c.cpy(fields[1], fields[2])
+	case "inc":
+		c.inc(fields[1])
+	case "dec":
+		c.dec(fields[1])
+	case "jnz":
+		c.jnz(fields[1], fields[2])
+	case "tgl":
+		c.tgl(fields[1])
+	}
+}
+
+func (c *y16d23Computer) tgl(i string) {
+	x := y16d23ValueOrRegister(c.registers, i)
+	mi := c.cursor + x
+	if mi < c.size {
+		newOp := ""
+		mfields := strings.Fields(c.instructions[mi])
+		if len(mfields) == 2 {
+			newOp = "inc"
+			if mfields[0] == "inc" {
+				newOp = "dec"
+			}
+		}
+		if len(mfields) == 3 {
+			newOp = "jnz"
+			if mfields[0] == "jnz" {
+				newOp = "cpy"
+			}
+		}
+		mfields[0] = newOp
+		c.instructions[mi] = strings.Join(mfields, " ")
+	}
+	c.cursor++
+}
+
+func (c *y16d23Computer) cpy(i1, i2 string) {
+	c.registers[i2] = y16d23ValueOrRegister(c.registers, i1)
+	c.cursor++
+}
+
+func (c *y16d23Computer) inc(i string) {
+	c.registers[i]++
+	c.cursor++
+}
+
+func (c *y16d23Computer) dec(i string) {
+	c.registers[i]--
+	c.cursor++
+}
+
+func (c *y16d23Computer) jnz(i1, i2 string) {
+	if y16d23ValueOrRegister(c.registers, i1) != 0 {
+		c.cursor += y16d23ValueOrRegister(c.registers, i2)
+	} else {
+		c.cursor++
+	}
+}
+
+var y16d23Registers = map[string]int{"a": 7, "b": 0, "c": 0, "d": 0}
 
 func y16d23(input string, registers map[string]int) int {
 	instructions := strings.Split(input, "\n")
-	cursor := 0
-	for cursor < len(instructions) {
-		// if cursor > 24 {
-		// 	log.Fatal("cursor", cursor)
-		// }
-		log.Println("registers", registers, "cursor", cursor, "instruction", instructions[cursor])
-		instruction := instructions[cursor]
-		fields := strings.Fields(instruction)
-		switch fields[0] {
-		case "cpy":
-			y16d23Copy(registers, fields[1], fields[2])
-		case "inc":
-			y16d23Inc(registers, fields[1])
-		case "dec":
-			y16d23Dec(registers, fields[1])
-		case "jnz":
-			cursor += y16d23JumpNotZero(registers, fields[1], fields[2])
-		case "tgl":
-			instructions = y16d23Toggle(registers, instructions, cursor)
-		}
-		cursor++
+	p := &y16d23Computer{
+		registers:    registers,
+		instructions: instructions,
+		cursor:       0,
+		size:         len(instructions),
 	}
-	return registers["a"]
-}
-
-func y16d23Toggle(registers map[string]int, instructions []string, cursor int) []string {
-	modIndex := cursor + y16d23ValueOrRegister(registers, instructions[cursor])
-	if modIndex < len(instructions) {
-		modInstructionFields := strings.Fields(instructions[modIndex])
-		if len(modInstructionFields) == 2 {
-			if modInstructionFields[0] == "inc" {
-				modInstructionFields[0] = "dec"
-			} else {
-				modInstructionFields[0] = "inc"
-			}
+	i := 0
+	for p.cursor < p.size {
+		i++
+		p.executeInstruction()
+		if i%50_000 == 0 {
+			log.Println("i", i, "cursor", p.cursor, "registers", p.registers)
 		}
-		if len(modInstructionFields) == 3 {
-			if modInstructionFields[0] == "jnz" {
-				modInstructionFields[0] = "cpy"
-			} else {
-				modInstructionFields[0] = "jnz"
-			}
-		}
-		instructions[modIndex] = strings.Join(modInstructionFields, " ")
 	}
-	return instructions
-}
-
-func y16d23JumpNotZero(registers map[string]int, i1, i2 string) (delta int) {
-	switch y16d23ValueOrRegister(registers, i1) {
-	case 0:
-		delta = 0
-	default:
-		delta = strToInt(i2) - 1
-	}
-	return
+	return p.registers["a"]
 }
 
 func y16d23ValueOrRegister(registers map[string]int, insPart string) (response int) {
@@ -74,16 +103,4 @@ func y16d23ValueOrRegister(registers map[string]int, insPart string) (response i
 		response = strToInt(insPart)
 	}
 	return
-}
-
-func y16d23Copy(registers map[string]int, i1, i2 string) {
-	registers[i2] = y16d23ValueOrRegister(registers, i1)
-}
-
-func y16d23Inc(registers map[string]int, i string) {
-	registers[i]++
-}
-
-func y16d23Dec(registers map[string]int, i string) {
-	registers[i]--
 }
--- go/adventofcode/y16d23_test.go
+++ go/adventofcode/y16d23_test.go
@@ -1,5 +1,10 @@
 package adventofcode
 
+import (
+	"strings"
+	"testing"
+)
+
 const y16d23TestInput string = `cpy 2 a
 tgl a
 tgl a
@@ -7,3 +12,64 @@ tgl a
 cpy 1 a
 dec a
 dec a`
+
+func Test_y16d23Processing(t *testing.T) {
+	c := y16d23Computer{
+		registers:    map[string]int{"a": 0, "b": 0, "c": 0, "d": 0},
+		instructions: strings.Split(y16d23TestInput, "\n"),
+		cursor:       0,
+		size:         len(strings.Split(y16d23TestInput, "\n")),
+	}
+
+	// 1: cpy 2 a
+	c.executeInstruction()
+	if c.registers["a"] != 2 {
+		t.Errorf("expected register a to be 2, got %d", c.registers["a"])
+	}
+
+	// 2: tgl a
+	c.executeInstruction()
+	if c.instructions[3] != "inc a" {
+		t.Errorf("expected instruction to be 'inc a', got %s", c.instructions[3])
+	}
+
+	// 3: tgl a
+	c.executeInstruction()
+	if c.instructions[4] != "jnz 1 a" {
+		t.Errorf("expected instruction to be 'jnz 1 a', got %s", c.instructions[4])
+	}
+
+	// 4: inc q
+	c.executeInstruction()
+	if c.registers["a"] != 3 {
+		t.Errorf("expected register a to be 3, got %d", c.registers["a"])
+	}
+
+	// 5: jnz 1 a
+	c.executeInstruction()
+	if c.cursor != 7 {
+		t.Errorf("expected cursor to be 8, got %d", c.cursor)
+	}
+
+}
+
+func Test_y16d23(t *testing.T) {
+	type args struct {
+		input     string
+		registers map[string]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"test p1", args{input: y16d23TestInput, registers: map[string]int{"a": 0, "b": 0, "c": 0, "d": 0}}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := y16d23(tt.args.input, tt.args.registers); got != tt.want {
+				t.Errorf("y16d23() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
