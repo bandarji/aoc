de9c092e391868c1a49760dc14b9ddf81e94962c
Sean Jain Ellis <sellis@bandarji.com>
2026-04-25 07:42:32 -0700

    2016 Day 24: Air Duct Spelunking (p1)

--- go/adventofcode/y16d24.go
+++ go/adventofcode/y16d24.go
@@ -7,66 +7,87 @@ import (
 	"strings"
 )
 
+const Ugh string = `###########
+#0.1.....2#
+#.#######.#
+#4.......3#
+###########`
+
+type y16d24AirDuct struct {
+	start image.Point
+	stops map[image.Point]bool
+	path  map[image.Point]bool
+}
+
 func y16d24(input string, part int) (answer int) {
-	start, stops, path := y16d24ParseInput(input)
-	// log.Println("start", start, "stops", stops)
-	answer = y16d24FindShortestPath(start, stops, path, part)
+	duct := y16d24ParseInput(input)
+	answer = y16d24Spelunk(duct, part)
 	return
 }
 
 type y16d24State struct {
-	pos          image.Point
-	steps        int
-	destinations string
+	pos         image.Point
+	steps       int
+	checkpoints map[image.Point]bool
 }
 
-func y16d24FindShortestPath(start image.Point, stops, path map[image.Point]bool, part int) (steps int) {
-	visited := map[image.Point]bool{}
-	destinations := ""
-	q := []y16d24State{{pos: start, steps: 0, destinations: destinations}}
+func (s y16d24State) encode() string {
+	checkpoints := []string{}
+	for k := range s.checkpoints {
+		checkpoints = append(checkpoints, fmt.Sprintf("[%d,%d]", k.X, k.Y))
+	}
+	return fmt.Sprintf("%d,%d,%s", s.pos.X, s.pos.Y, strings.Join(checkpoints, ","))
+}
+
+func y16d24Spelunk(duct y16d24AirDuct, part int) int {
+	cache := map[string]bool{}
+	if part == 2 {
+		return -1
+	}
+	q := []y16d24State{{pos: duct.start, steps: 0, checkpoints: map[image.Point]bool{duct.start: true}}}
 	for len(q) > 0 {
 		current := q[0]
 		q = q[1:]
-		// log.Println("current", current)
-		if visited[current.pos] {
+		encoded := current.encode()
+		if cache[encoded] {
 			continue
 		}
-		visited[current.pos] = true
-		if len(current.destinations) == len(stops) {
-			log.Println("current", current.destinations, "stops", stops, "steps", current.steps)
-			steps = current.steps
-			break
+		cache[encoded] = true
+		if len(current.checkpoints) == len(duct.stops) {
+			return current.steps
 		}
 		for _, dir := range [4]image.Point{{0, 1}, {1, 0}, {0, -1}, {-1, 0}} {
 			next := current.pos.Add(dir)
-			if path[next] {
-				destinations := current.destinations
-				if stops[next] {
-					destinations = fmt.Sprintf("%s%d", destinations, next)
+			if duct.path[next] {
+				np := map[image.Point]bool{}
+				for k := range current.checkpoints {
+					np[k] = true
 				}
-				q = append(q, y16d24State{pos: next, steps: current.steps + 1, destinations: destinations})
+				if duct.stops[next] {
+					np[next] = true
+				}
+				q = append(q, y16d24State{pos: next, steps: current.steps + 1, checkpoints: np})
 			}
 		}
 	}
-	return
+	log.Println("no solution found")
+	return 0
 }
 
-func y16d24ParseInput(input string) (image.Point, map[image.Point]bool, map[image.Point]bool) {
-	stops := map[image.Point]bool{}
-	start := image.Point{}
-	path := map[image.Point]bool{}
+func y16d24ParseInput(input string) y16d24AirDuct {
+	duct := y16d24AirDuct{stops: map[image.Point]bool{}, path: map[image.Point]bool{}}
 	for y, line := range strings.Split(input, "\n") {
 		for x, cell := range line {
-			switch cell {
-			case '0':
-				start = image.Point{x, y}
-				stops[image.Point{x, y}] = true
-			case '1', '2', '3', '4', '5', '6', '7', '8', '9':
-				stops[image.Point{x, y}] = true
-			case '.':
-				path[image.Point{x, y}] = true
+			if cell != '#' {
+				duct.path[image.Point{x, y}] = true
+				if cell >= '0' && cell <= '9' {
+					duct.stops[image.Point{x, y}] = true
+				}
+				if cell == '0' {
+					duct.start = image.Point{x, y}
+				}
 			}
 		}
 	}
-	return start, stops, path
+	return duct
 }
