27170311984175b575928101d7d2ee1aaadae7ac
Sean Jain Ellis <sellis@bandarji.com>
2026-04-30 14:25:51 -0700

    2016 day 11 will end up killing me

--- go/adventofcode/y16d11.go
+++ go/adventofcode/y16d11.go
@@ -2,83 +2,53 @@ package adventofcode
 
 import (
 	"fmt"
-	"log"
 	"sort"
 	"strings"
 )
 
-type y16d11Floor struct {
-	items map[string]bool
+type y16d11Item struct {
+	name string
+	chip bool // false for generator, true for chip
 }
 
 type y16d11State struct {
-	elevator, steps, floorCount int
-	floors                      map[int]y16d11Floor
+	floors          [][]y16d11Item
+	elevator, steps int
 }
 
-func (s *y16d11State) moves() []y16d11State {
-	states := []y16d11State{}
-	newState := s.clone()
-	for item := range s.floors[s.elevator].items {
-		for delta := range []int{-1, 1} {
-			newState = s.clone()
-			newState.moveItem(delta, item, s.elevator)
-			if newState.isValid() {
-				newState.steps++
-				newState.elevator += delta
-				states = append(states, newState)
-			}
-			newState = s.clone()
-			if strings.HasSuffix(item, "C") && newState.floors[s.elevator].items[fmt.Sprintf("%sG", item[0:1])] {
-				newState.moveItems(delta, item, fmt.Sprintf("%sG", item[0:1]), s.elevator)
-			}
-			if newState.isValid() {
-				newState.steps++
-				newState.elevator += delta
-				states = append(states, newState)
-			}
-		}
+func (s y16d11State) clone() y16d11State {
+	sc := y16d11State{
+		floors:   [][]y16d11Item{},
+		elevator: s.elevator,
+		steps:    s.steps,
 	}
-	return states
-}
-
-func (s *y16d11State) moveItem(delta int, item string, elevator int) {
-	log.Println("MOVING ITEM", item, "FROM", elevator, "TO", elevator+delta)
-	delete(s.floors[elevator].items, item)
-	s.floors[elevator+delta].items[item] = true
-	s.elevator += delta
-}
-
-func (s *y16d11State) moveItems(delta int, item string, partner string, elevator int) {
-	log.Println("MOVING ITEMS", item, "AND", partner, "FROM", elevator, "TO", elevator+delta)
-	delete(s.floors[elevator].items, item)
-	delete(s.floors[elevator].items, partner)
-	s.floors[elevator+delta].items[item] = true
-	s.floors[elevator+delta].items[partner] = true
-	s.elevator += delta
-}
-
-func (s *y16d11State) predict() []y16d11State {
-	states := []y16d11State{}
-	states = append(states, s.moves()...)
-	return states
-}
-
-func (s *y16d11State) clone() y16d11State {
-	newState := y16d11State{elevator: s.elevator, steps: s.steps, floorCount: s.floorCount, floors: map[int]y16d11Floor{}}
-	for f, floor := range s.floors {
-		newState.floors[f] = y16d11Floor{items: map[string]bool{}}
-		for item := range floor.items {
-			newState.floors[f].items[item] = true
-		}
+	for _, f := range s.floors {
+		sc.floors = append(sc.floors, append([]y16d11Item{}, f...))
 	}
-	return newState
+	return sc
+}
+
+func (s y16d11State) done() bool {
+	total := 0
+	for _, floor := range s.floors[:len(s.floors)-1] {
+		total += len(floor)
+	}
+	return total == 0
 }
 
 func (s y16d11State) isValid() bool {
-	for f := 1; f < s.floorCount; f++ {
-		for item := range s.floors[f].items {
-			if strings.HasSuffix(item, "C") && !s.floors[f].items[fmt.Sprintf("%sG", item[0:1])] {
+	for f := range s.floors {
+		gens := map[string]bool{}
+		for _, item := range s.floors[f] {
+			if !item.chip {
+				gens[item.name] = true
+			}
+		}
+		if len(gens) == 0 {
+			continue
+		}
+		for _, item := range s.floors[f] {
+			if item.chip && !gens[item.name] {
 				return false
 			}
 		}
@@ -86,82 +56,109 @@ func (s y16d11State) isValid() bool {
 	return true
 }
 
-func (s y16d11State) encode() string {
+func (s y16d11State) nextStates() (states []y16d11State) {
+	states = []y16d11State{}
+	moves := [][]int{}
+	items := s.floors[s.elevator]
+	for i := 0; i < len(items); i++ {
+		for j := i + 1; j < len(items); j++ {
+			moves = append(moves, []int{i, j})
+		}
+	}
+	for i := range items {
+		moves = append(moves, []int{i})
+	}
+	elevatorDeltas := []int{}
+	if s.elevator < len(s.floors)-1 {
+		elevatorDeltas = append(elevatorDeltas, 1)
+	}
+	if s.elevator > 0 {
+		elevatorDeltas = append(elevatorDeltas, -1)
+	}
+	for _, ed := range elevatorDeltas {
+		for _, move := range moves {
+			sc := s.clone()
+			sc.elevator += ed
+			sc.steps++
+			prevFloor := s.elevator
+			newFloor := sc.elevator
+			for _, index := range move {
+				sc.floors[newFloor] = append(sc.floors[newFloor], sc.floors[prevFloor][index])
+			}
+			for in := len(move) - 1; in >= 0; in-- {
+				sc.floors[prevFloor][move[in]] = sc.floors[prevFloor][len(sc.floors[prevFloor])-1]
+				sc.floors[prevFloor] = sc.floors[prevFloor][:len(sc.floors[prevFloor])-1]
+			}
+			if sc.isValid() {
+				states = append(states, sc)
+			}
+		}
+	}
+	return
+}
+
+func (s y16d11State) hash() string {
 	sb := strings.Builder{}
-	sb.WriteString(fmt.Sprintf("e%d", s.elevator))
-	for _, k := range y16d11SortFloorKeys(s.floors) {
-		sb.WriteString(fmt.Sprintf("|f%d|", k))
-		sb.WriteString(strings.Join(y16d11SortFloorItemKeys(s.floors[k]), ""))
+	sb.WriteString(fmt.Sprintf("[%d]", s.elevator))
+	for f, floor := range s.floors {
+		chips := []string{}
+		generators := []string{}
+		for _, item := range floor {
+			if item.chip {
+				chips = append(chips, item.name)
+			} else {
+				generators = append(generators, item.name)
+			}
+		}
+		sort.Strings(chips)
+		sort.Strings(generators)
+		sb.WriteString(fmt.Sprintf("%d:%s:%s:", f, strings.Join(chips, ","), strings.Join(generators, ",")))
 	}
 	return sb.String()
 }
 
-func y16d11SortFloorKeys(floors map[int]y16d11Floor) []int {
-	keys := []int{}
-	for k := range floors {
-		keys = append(keys, k)
-	}
-	sort.Ints(keys)
-	return keys
-}
-
-func y16d11SortFloorItemKeys(floor y16d11Floor) []string {
-	keys := []string{}
-	for k := range floor.items {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-	return keys
-}
-
-func y16d11ParseInput(input string) (state y16d11State) {
-	state = y16d11State{elevator: 1, steps: 0, floors: map[int]y16d11Floor{}}
-	for f, line := range strings.Split(input, "\n") {
-		items := map[string]bool{}
+func y16d11ParseInput(input string, part int) (state y16d11State) {
+	state = y16d11State{floors: [][]y16d11Item{}, elevator: 1, steps: 0}
+	for _, line := range strings.Split(input, "\n") {
+		items := []y16d11Item{}
 		fields := strings.Fields(line)
 		for i := range len(fields) {
 			if fields[i] == "a" {
 				if strings.Contains(fields[i+1], "-") {
-					items[fmt.Sprintf("%sC", strings.ToUpper(fields[i+1][0:1]))] = true
+					items = append(items, y16d11Item{name: strings.ToUpper(strings.Split(fields[i+1], "-")[0]), chip: false})
 				} else {
-					items[fmt.Sprintf("%sG", strings.ToUpper(fields[i+1][0:1]))] = true
+					items = append(items, y16d11Item{name: strings.ToUpper(strings.Split(fields[i+1], "-")[0]), chip: true})
 				}
 			}
 		}
-		state.floors[f] = y16d11Floor{items: items}
+		state.floors = append(state.floors, items)
 	}
-	state.floorCount = len(state.floors)
+	if part == 2 {
+		state.floors[0] = append(state.floors[0], y16d11Item{name: "ELERIUM", chip: false})
+		state.floors[0] = append(state.floors[0], y16d11Item{name: "ELERIUM", chip: true})
+		state.floors[0] = append(state.floors[0], y16d11Item{name: "DILITHIUM", chip: false})
+		state.floors[0] = append(state.floors[0], y16d11Item{name: "DILITHIUM", chip: true})
+	}
 	return
 }
 
 func y16d11(input string, part int) int {
-	_ = part
+	// log.Printf("state: %+v", state)
+	q := []y16d11State{y16d11ParseInput(input, part)}
 	cache := map[string]bool{}
-	state := y16d11ParseInput(input)
-	q := []y16d11State{state}
 	for len(q) > 0 {
 		current := q[0]
 		q = q[1:]
-		if current.isDone() {
+		if current.done() {
 			return current.steps
 		}
-		hash := current.encode()
-		log.Println("HASH", hash)
+		hash := current.hash()
 		if cache[hash] {
 			continue
 		}
 		cache[hash] = true
-		states := current.predict()
+		states := current.nextStates()
 		q = append(q, states...)
 	}
 	return -1
-}
-
-func (s y16d11State) isDone() bool {
-	for f := 1; f < s.floorCount-1; f++ {
-		if len(s.floors[f].items) > 0 {
-			return false
-		}
-	}
-	return true
 }
