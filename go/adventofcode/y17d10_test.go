d8a2865333496356738a44f28f1d1853f52efbae
Sean Jain Ellis <sellis@bandarji.com>
2026-06-11 17:14:20 +0300

    2017 Day 10: Knot Hash

--- go/adventofcode/y17d10_test.go
+++ go/adventofcode/y17d10_test.go
@@ -13,9 +13,13 @@ func Test_y17d10(t *testing.T) {
 	tests := []struct {
 		name        string
 		args        args
-		wantProduct int
+		wantProduct string
 	}{
-		{"test p1", args{input: y17d10TestInput, listLength: 5, part: 1}, 12},
+		{"test p1", args{input: y17d10TestInput, listLength: 5, part: 1}, "12"},
+		{"test p2", args{input: "", listLength: 256, part: 2}, "a2582a3a0e66e6e86e3812dcb672a272"},
+		{"test p2", args{input: "AoC 2017", listLength: 256, part: 2}, "33efeb34ea91902bb2f59c9920caa6cd"},
+		{"test p2", args{input: "1,2,3", listLength: 256, part: 2}, "3efbe78a8d82f29979031a4aa0b16a9d"},
+		{"test p2", args{input: "1,2,4", listLength: 256, part: 2}, "63960835bcdc130f0b66d7ff4f6a5a8e"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
